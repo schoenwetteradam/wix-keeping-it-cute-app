38bfdbfd2e90af133461bca46d7634091a399682
schoenwetteradam <103909524+schoenwetteradam@users.noreply.github.com>
2025-06-27 17:56:12 -0500

    Wrap Home page with fragment

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // pages/index.js - Simple redirect to staff portal
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 export default function Home() {
   const router = useRouter()
@@ -10,9 +11,14 @@ export default function Home() {
   }, [router])
   
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Keeping It Cute Salon</h1>
-      <p>Redirecting to staff portal...</p>
-    </div>
+    <>
+      <Head>
+        <title>Home - Keeping It Cute Salon</title>
+      </Head>
+      <div style={{ padding: '20px', textAlign: 'center' }}>
+        <h1>Keeping It Cute Salon</h1>
+        <p>Redirecting to staff portal...</p>
+      </div>
+    </>
   )
 }
