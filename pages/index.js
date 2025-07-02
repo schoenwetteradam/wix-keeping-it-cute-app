2ea2e94e744046116256c9f64f598fadcbf80584
schoenwetteradam <103909524+schoenwetteradam@users.noreply.github.com>
2025-07-01 22:48:54 -0500

    Enable SVGR and add example icon usage

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
+import Image from 'next/image'
+import { PlusIcon } from '../components/icons'
 
 export default function Home() {
   const router = useRouter()
@@ -16,7 +18,16 @@ export default function Home() {
         <title>Home - Keeping It Cute Salon</title>
       </Head>
       <div style={{ padding: '20px', textAlign: 'center' }}>
+        <PlusIcon width={32} height={32} style={{ color: '#ff69b4' }} />
         <h1>Keeping It Cute Salon</h1>
+        <Image
+          src="/glory.svg"
+          alt="Decorative graphic"
+          width={300}
+          height={200}
+          loading="lazy"
+          style={{ margin: '20px auto' }}
+        />
         <p>Redirecting to staff portal...</p>
       </div>
     </>
