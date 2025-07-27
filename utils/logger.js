d3c63624eeef4d1f1355b8b82c763388676bf054
schoenwetteradam <103909524+schoenwetteradam@users.noreply.github.com>
2025-07-27 12:12:08 -0500

    Add logger utility and use in upload product image

---
+++ utils/logger.js
@@ -0,0 +1,8 @@
+const logger = {
+  debug: (...args) => console.debug('[DEBUG]', ...args),
+  info: (...args) => console.info('[INFO]', ...args),
+  warn: (...args) => console.warn('[WARN]', ...args),
+  error: (...args) => console.error('[ERROR]', ...args)
+}
+
+export default logger
